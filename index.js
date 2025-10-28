a6128ef3ccb50553c8c7ed7768e493b6cb45d6b6
abimanyusatriaa <adji3890@gmail.com>
2025-10-28 15:03:44 +0700

    membuat db.sequelize

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+db.sequelize.sync(). then((result) => {
+    app.listen(3000, () =>  {
+        console.log('Server started');
+    })
+})
+    .catch((err) => {
+        console.log(err);
+})
+
